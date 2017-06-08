34ffc0c894a842697b24345eea972169a65bf4c0
Loïc Hoguin <essen@ninenines.eu>
2017-06-08 11:37:43 +0200

    Add copyright headers

--- c_src/lg_tracer.c
+++ c_src/lg_tracer.c
@@ -1,17 +1,16 @@
-// @todo Fix this copyright, it's incorrect.
-// Copyright (c) 2014-2015, Loïc Hoguin <essen@ninenines.eu>
+// Copyright (c) 2017-Present Pivotal Software, Inc.  All rights reserved.
 //
-// Permission to use, copy, modify, and/or distribute this software for any
-// purpose with or without fee is hereby granted, provided that the above
-// copyright notice and this permission notice appear in all copies.
+// This package, Looking Glass, is double-licensed under the Mozilla
+// Public License 1.1 ("MPL") and the Apache License version 2
+// ("ASL"). For the MPL, please see LICENSE-MPL-RabbitMQ. For the ASL,
+// please see LICENSE-APACHE2.
 //
-// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+// This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+// either express or implied. See the LICENSE file for specific language governing
+// rights and limitations of this software.
+//
+// If you have any questions regarding licensing, please contact us at
+// info@rabbitmq.com.
 
 #define NIF_FUNCTION_NAME(f) lg_ ## f
 
--- src/lg_callgrind.erl
+++ src/lg_callgrind.erl
@@ -1,3 +1,17 @@
+%% Copyright (c) 2017-Present Pivotal Software, Inc.  All rights reserved.
+%%
+%% This package, Looking Glass, is double-licensed under the Mozilla
+%% Public License 1.1 ("MPL") and the Apache License version 2
+%% ("ASL"). For the MPL, please see LICENSE-MPL-RabbitMQ. For the ASL,
+%% please see LICENSE-APACHE2.
+%%
+%% This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+%% either express or implied. See the LICENSE file for specific language governing
+%% rights and limitations of this software.
+%%
+%% If you have any questions regarding licensing, please contact us at
+%% info@rabbitmq.com.
+
 -module(lg_callgrind).
 
 -export([patterns/0]).
